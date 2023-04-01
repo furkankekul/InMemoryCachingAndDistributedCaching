1217ecb292120d5e24a1b0e32e82b9e0d081b484
furkankekul <kekulfurkan@gmail.com>
2023-04-02 00:23:44 +0300

    InMemoryCachingAndDistributedCachingExample
    InMemory ve Distributed Caching işlemleri basit bir şekilde anlatılmıştır.

---
+++ RedisDemo1/DistributedCaching/Controllers/ValuesController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Distributed;
+using System.Text;
+
+namespace DistributedCaching.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        #region Distributed Cache İşlem Sırası
+        //İlk olarak uygulamaya StackExchangesRedis kütüphaesini indiriyoruz.
+        //Daha sonra AddStackExchangesRedisCache servisini uygulamaya ekliyoruz.
+        //Daha sonra IDistributedCache referansını DependencyIncetion ile injekt ediyoruz.
+        //SetString metotu ile metinsel,Set metotu ile binary olarak verileriniz redis'e cacheleyebilirisiniz.
+        //GetString ve Get metotu ile cachelenmiş verileri elde edebilirsiniz.
+        //Remove Fonksiyonu ile cache'lenmiş verileri silebilirsiniz veya sliding time ve absolute time konfigürasyonları yaparak cachede ki veriye bir zaman aralığı sunara manuel değil de 
+        //belirlediğiniz zaman aralığında silinmesini sağlayabilirsiniz.
+        #endregion
+        readonly IDistributedCache _distributedCache;
+        public ValuesController(IDistributedCache distributedCache)
+        {
+            _distributedCache = distributedCache;
+        }
+        [HttpGet("set")]
+        public void Set(string name)
+        {
+            byte[] value = Encoding.UTF8.GetBytes(name);
+            _distributedCache.Set("NAME", value, options: new DistributedCacheEntryOptions()
+            {
+                AbsoluteExpiration = DateTime.Now.AddSeconds(60),//Data maximum 60 saniye cache'de kalacak sonra kesinlikle silinecek
+                SlidingExpiration = TimeSpan.FromSeconds(10) // Data 10 saniye içinde bir istek gelirse cache de kalma süresi uzayacaka ama max 60 saniye uzar 
+                //Eğer ki 10 sn içerisinde bir istek gelmezse data cache den kesinlikles silinecek.
+
+            });
+        }
+
+        [HttpGet("get")]
+        public byte[] Get()
+        {
+            return _distributedCache.Get("NAME");
+        }
+
+
+
+
+
+    }
+}
---
+++ RedisDemo1/RedisDemo1/Controllers/ValuesController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
+
+namespace InMemoryCaching.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        #region In-Memory Cache İşlem Sırası
+        //ilk olarak uygulamaya AddMemoryCache servisini ekleyiniz(Bu işlemi uygulamanın program.cs sınıfında builder.Services.AddMemoryCache() olarak yap!!)
+        //Daha sonra dependency Incetion ile IMemoryCache den bir instance üretiniz
+        //Set metotu ile veriyi cacheleyebilir,Get metotuyla cache'lenmiş veriyi elde edebilirsiniz
+        //Remove fonksiyonu ile cachelenmiş veriyi silebilirsiniz
+        //TryGetValue metotu ile kontrollü bir şekilde cache'den veriyi okuyabilirsiniz.
+        //Aşağı da kod üzerinden adımları uygulamalı bir şekilde en basit haliyle In-Memory Caching'i inceleyebilirsin.
+        #endregion
+
+        readonly IMemoryCache _memoryCache;
+        public ValuesController(IMemoryCache memoryCache)
+        {
+            _memoryCache = memoryCache;
+        }
+
+        [HttpGet("Set")]
+        public void Set(string name)
+        {
+            _memoryCache.Set("name",name);
+        }
+
+        [HttpGet]
+        public string Get()
+        {
+            if (_memoryCache.TryGetValue<string>("name", out string value))
+            {
+                return _memoryCache.Get<string>("name");
+            }
+            return string.Empty;    
+
+            
+        }
+
+
+    }
+}
